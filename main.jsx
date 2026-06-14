60934088a2a3eb06a93832e2f79e22bf66689638
SMadzharov <98599735+SMadzharov@users.noreply.github.com>
2026-06-15 00:28:28 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Публични данни за връзка към твоя Supabase проект (безопасни за сайта).
-const SUPABASE_URL = "https://iiewjrqusbdkekmrgwqt.supabase.co";
-const SUPABASE_KEY = "sb_publishable_3pvOLB1B5MV3sRXIvI7zTA_PssLwA27";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
